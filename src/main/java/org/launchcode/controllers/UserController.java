65800d17e9c11077054b7668e516f07881bf0476
MVDLNE <madelinelora95@icloud.com>
2024-04-11 23:48:44 -0400

    modifying user class by persisting user object

--- src/main/java/org/launchcode/controllers/UserController.java
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -1,8 +1,10 @@
 package org.launchcode.controllers;
 
+import jakarta.validation.Valid;
 import org.launchcode.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,24 +12,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
-@RequestMapping("/user")
+@RequestMapping("user")
 public class UserController {
 
     @GetMapping("/add")
-    public String displayAddUserForm() {
+    public String displayAddUserForm(Model model) {
+        model.addAttribute(new User());
         return "user/add";
     }
 
-    @PostMapping("")
-    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
-        if (!user.getPassword().equals(verify)){
-            model.addAttribute("username", user.getUsername());
-            model.addAttribute("email", user.getEmail());
-            model.addAttribute("error", "Passwords do not match");
-            return "/user/add";
+    @PostMapping()
+    public String processAddUserForm(Model model, @ModelAttribute @Valid User user, Errors errors) {
+        if (!errors.hasErrors()){
+            return "user/index";
+        } else {
+            return "user/add";
         }
-        model.addAttribute("user", user);
-        return "/user/index";
+
     }
 
 }
--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,38 +1,64 @@
 package org.launchcode.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Entity
 public class User {
 
+
+    @Size(min = 5, max = 15, message = "Username must be between 5-15 characters long")
+    @NotNull(message = "Username is required.")
     private String username;
+
+    @NotNull(message = "Password is required")
+    @Size(min = 6, max = 15, message = "Password must be 6 characters long")
+    private String pwHash;
+
+    @Email
     private String email;
-    private String password;
+
+
+    @NotNull(message = "Passwords do not match AT ALL")
+    @Size(min = 6, max = 15)
+    private String verifyPassword;
+
+    public User(){
+
+    }
 
     public User(String username, String email, String password) {
         this.username = username;
         this.email = email;
-        this.password = password;
+        this.pwHash = encoder.encode(password);
     }
+
+
+//    public User(String username, String email, String password, String verifyPassword) {
+//        this();
+//        this.username = username;
+//        this.email = email;
+//        this.password = password;
+//        this.verifyPassword = verifyPassword;
+//    }
 
     public String getUsername() {
         return username;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    public boolean isMatchingPassword(String password) {
+        return encoder.matches(password, pwHash);
     }
 
-    public String getPassword() {
-        return password;
-    }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
