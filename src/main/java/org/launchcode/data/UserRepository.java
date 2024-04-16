14536f94a6de3308c97dc7d0e9ea4cfa78204040
MVDLNE <madelinelora95@icloud.com>
2024-04-16 00:01:37 -0400

    added functionality for users to log in and out of the application

---
+++ src/main/java/org/launchcode/data/UserRepository.java
@@ -0,0 +1,12 @@
+package org.launchcode.data;
+
+import org.launchcode.models.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+    User findByUsername(String username);
+
+}
--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -3,36 +3,27 @@ package org.launchcode.models;
 import jakarta.persistence.Entity;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Entity
-public class User {
+public class User extends AbstractEntity{
 
 
-    @Size(min = 5, max = 15, message = "Username must be between 5-15 characters long")
-    @NotNull(message = "Username is required.")
+    @NotNull
     private String username;
 
-    @NotNull(message = "Password is required")
-    @Size(min = 6, max = 15, message = "Password must be 6 characters long")
+    @NotNull
     private String pwHash;
 
     @Email
     private String email;
 
-
-    @NotNull(message = "Passwords do not match AT ALL")
-    @Size(min = 6, max = 15)
-    private String verifyPassword;
-
     public User(){
 
     }
 
-    public User(String username, String email, String password) {
+    public User(String username, String password) {
         this.username = username;
-        this.email = email;
         this.pwHash = encoder.encode(password);
     }
 
