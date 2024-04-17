b411a6cec69d068dbaaa7d29401f65fe52565f01
MVDLNE <madelinelora95@icloud.com>
2024-04-16 22:53:22 -0400

    implemented request filter to enable basic authentication

--- src/main/java/org/launchcode/controllers/HomeController.java
+++ src/main/java/org/launchcode/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package org.launchcode.controllers;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +8,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+//    @GetMapping("/")
+//    public String renderHomePage(Model model) {
+//        model.addAttribute("headingText", "Welcome");
+//        return "index";
+//    }
     @GetMapping("/")
-    public String renderHomePage(Model model) {
-        model.addAttribute("headingText", "Welcome");
+    public String redirectHomePage() {
+        return "redirect:/welcome";
+    }
+
+    @GetMapping("/welcome")
+    public String renderHomePage(Model model, HttpSession session) {
+        model.addAttribute("loggedIn", session.getAttribute("user") != null);
         return "index";
     }
 
